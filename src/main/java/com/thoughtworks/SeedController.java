5c9d0a495aeb0f62c3237c32b2988fdbc97f841f
Karan Krishnani <krkrishn@thoughtworks.com>
2017-03-17 13:54:42 -0400

    passing test for case when there are no seeds

--- src/main/java/com/thoughtworks/SeedController.java
+++ src/main/java/com/thoughtworks/SeedController.java
@@ -1,10 +1,29 @@
 package com.thoughtworks;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by krkrishn on 3/17/17.
  */
 @RestController
 public class SeedController {
+
+
+    @RequestMapping("/seeds")
+    public ResponseEntity<Map<String, String>> getSeeds() {
+        Map<String, String> resultMap = new HashMap<>();
+        resultMap.put("error", "not found");
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(resultMap);
+    }
 }
